b696ec56c858af3f52b15d53c66346ef27b7b2a2
jentino <jentino@gmail.com>
2018-09-19 15:34:28 +0000

    committtiingg

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mzansi Forex Class' });
+  res.render('index', 
+    { 
+        title: 'Mzansi Forex Academy',
+        message: 'Welcome to Mzansi Forex Academy'
+    });
 });
 
 module.exports = router;
